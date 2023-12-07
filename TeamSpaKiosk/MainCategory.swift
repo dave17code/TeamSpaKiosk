04a434f48844185fb57579095d63ab12fc2944c5
dave17code <dave17code@gmail.com>
2023-12-08 00:49:19 +0900

    20231208First

--- TeamSpaKiosk/MainCategory.swift
+++ TeamSpaKiosk/MainCategory.swift
@@ -14,6 +14,11 @@ struct MenuCategory {
                 "2. Drinks          | 매장에서 직접 만드는 음료"]
     
     func menuPrint() {
+        print("======================================================================")
+        print("SHACKSHACK BUGER에 오신걸 환영합니다.")
+        print("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.")
+        print("======================================================================")
+        print("")
         print("[ SHAKESHAKE MENU ]")
         for m in menu {
             print(m)
--- TeamSpaKiosk/main.swift
+++ TeamSpaKiosk/main.swift
@@ -8,22 +8,23 @@ import Foundation
 
 var currentScreen = MenuCategory().name
 
-MenuCategory().menuPrint()
+//MenuCategory().menuPrint()
 
 func kioskStart() {
-    
-    let selectNumber = readLine()!
     
     // 메뉴 카테고리 선택
     if currentScreen == "MenuCategory" {
                     
+        MenuCategory().menuPrint()
+        let selectNumber = readLine()!
+
         switch selectNumber {
         case "1":
+            print("")
             currentScreen = Burgers().name
-            Burgers().menuPrint()
         case "2":
+            print("")
             currentScreen = Drinks().name
-            Drinks().menuPrint()
         default:
             print("잘못된 번호를 입력했어요. 다시 입력해주세요.")
         }
@@ -32,11 +33,33 @@ func kioskStart() {
     // 버거 메뉴 선택
     if currentScreen == "Burgers" {
         
+        Burgers().menuPrint()
         let selectNumber = readLine()!
-        
+
         switch selectNumber {
+
         case "1":
+            print("")
             print(Burgers().menu[0].dropFirst(3))
+            print("위 메뉴를 장바구니에 추가 하시겠습니까?")
+            print("1. 확인  2. 취소")
+            
+            let selectNumber = readLine()!
+
+            switch selectNumber {
+                
+            case "1":
+            print("")
+            print("장바구니에 추가 되었습니다.")
+            print("")
+            currentScreen = MenuCategory().name
+            case "2":
+            print("")
+            currentScreen = Burgers().name
+            default:
+                print("잘못된 번호를 입력했어요. 다시 입력해주세요.")
+            }
+            
         case "2":
             print(Burgers().menu[1].dropFirst(3))
         case "3":
@@ -44,8 +67,8 @@ func kioskStart() {
         case "4":
             print(Burgers().menu[3].dropFirst(3))
         case "0":
+            print("")
             currentScreen = MenuCategory().name
-            MenuCategory().menuPrint()
         default:
             print("잘못된 번호를 입력했어요. 다시 입력해주세요.")
         }
@@ -53,9 +76,10 @@ func kioskStart() {
     
     // 드링크 메뉴 선택
     if currentScreen == "Drinks" {
-        
+                
+        Drinks().menuPrint()
         let selectNumber = readLine()!
-        
+
         switch selectNumber {
         case "1":
             print(Drinks().menu[0].dropFirst(3))
@@ -66,8 +90,8 @@ func kioskStart() {
         case "4":
             print(Drinks().menu[3].dropFirst(3))
         case "0":
+            print("")
             currentScreen = MenuCategory().name
-            MenuCategory().menuPrint()
         default:
             print("잘못된 번호를 입력했어요. 다시 입력해주세요.")
         }
